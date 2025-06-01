d9f225d7861ba308c3e1393e3568477fc97e0a50
Derrick Bol <derrickbol33@gmail.com>
2025-06-01 18:25:53 +0800

    fix: add posthog proxy

--- client/providers/posthog-provider.tsx
+++ client/providers/posthog-provider.tsx
@@ -8,7 +8,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+        api_host: "/ingest",
         defaults: "2025-05-24",
       });
     }
