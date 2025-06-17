8f33c0e4409dda28061858076b251c1cb922a7d3
Derrick Bol <derrickbol33@gmail.com>
2025-06-17 18:33:59 +0800

    feat: init

--- client/ui/atoms/pace.tsx
+++
@@ -1,9 +0,0 @@
-import { PaceUnit } from "@/server/services/pace-calculator/schemas";
-
-export const Pace = ({ pace, unit }: { pace: string; unit: PaceUnit }) => {
-  return (
-    <span>
-      {pace} {unit === PaceUnit.Kilometers ? "min/km" : "min/mi"}
-    </span>
-  );
-};
